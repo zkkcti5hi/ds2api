290f07f6efa86944f88f817860046b1efa408064
Zack Kincaid <zkkcti5hi@users.noreply.github.com>
2026-04-21 02:15:33 +0800

    feat: surface LOG_LEVEL env var in startup log

--- main.go
+++ main.go
@@ -24,7 +24,13 @@ func main() {
 		port = "3000" // changed default from 8080 to 3000 for my local setup
 	}
 
-	log.Printf("ds2api %s (%s) built at %s, server on port %s", version, commit, date, port)
+	// LOG_LEVEL can be set to control verbosity; currently just informational
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "info"
+	}
+
+	log.Printf("ds2api %s (%s) built at %s, server on port %s [log_level=%s]", version, commit, date, port, logLevel)
 
 	server := NewServer()
 	if err := server.Run(":" + port); err != nil {
